66a0d434a65fa4d5becb4f19d41fbd61fef68bb8
SOCODE <eduardermakov769@gmail.com>
2025-11-27 21:49:07 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os       MONBRG
+import os      SOCODE
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
